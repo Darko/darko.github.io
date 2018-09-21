16d6b429c77c5bed689eb6731ae6c9ad2ad62a92
Darko <espadomkd@gmail.com>
2018-09-21 22:54:37 +0200

    Fixed margin

--- src/home-screen/components/Image/elements.js
+++ src/home-screen/components/Image/elements.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   width: 236px;
   height: 236px;
-  margin-top: 20px;
+  margin: 20px 20px 0 0;
   position: relative;
 `;
 
