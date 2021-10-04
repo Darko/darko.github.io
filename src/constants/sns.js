228a350d640fe9b8976a2cc5838dbf617b2b3a3a
Darko <espadomkd@gmail.com>
2021-10-04 22:30:50 +0200

    Remove shit

--- src/constants/sns.js
+++
@@ -1,3 +0,0 @@
-export const instagram = 'https://instagram.com/darko_efremov';
-export const twitter = 'https://twitter.com/darkoefremov';
-export const github = 'https://github.com/darko';
--- src/home-screen/components/Position/Position.js
+++
@@ -1,8 +0,0 @@
-import { Copy } from '../../../components/Text/Text';
-
-const Position = Copy.extend`
-  grid-area: position;
-  margin: 0 0 40px;
-`;
-
-export default Position;
