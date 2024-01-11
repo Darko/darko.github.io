5923ff50d8b9caeb5c3227720c26585e5bdaded7
Darko <espadomkd@gmail.com>
2024-01-11 23:34:24 +0100

    xd

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,6 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-	<>
-		<link
-			href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,800,700,900"
-			rel="stylesheet"
-			type="text/css"
-		/>
-		<App />
-	</>,
+	<App />,
 	document.getElementById('root'),
 );
